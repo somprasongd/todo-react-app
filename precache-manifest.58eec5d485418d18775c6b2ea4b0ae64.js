dc45848e6f80f6826e53f04120d395de765ee878
Somprasong Damyos <somprasong.damyos@gmail.com>
2019-07-04 10:02:53 +0700

    Updates

---
+++ precache-manifest.58eec5d485418d18775c6b2ea4b0ae64.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4736f4024332c730316ac1423c42cf",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "aaa800cb4f3154b2158b",
+    "url": "/todo-react-app/static/js/2.d232ec74.chunk.js"
+  },
+  {
+    "revision": "703c98812d8f9fca6f20",
+    "url": "/todo-react-app/static/js/main.f6b6906f.chunk.js"
+  },
+  {
+    "revision": "5fd7fe22f3e7a4aea29a",
+    "url": "/todo-react-app/static/js/runtime~main.7e9341e7.js"
+  }
+]);
