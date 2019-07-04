4f624fe286e5fa4682116e231113aae7179e8eec
Somprasong Damyos <somprasong.damyos@gmail.com>
2019-07-04 16:33:35 +0700

    update navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,29 +3,20 @@ import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Todo-list React
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/todos">
-            Todo
-          </NavLink>
-          <NavLink className="nav-item nav-link" to="/about">
-            About
-          </NavLink>
+    <nav className="navbar navbar-expand-sm navbar-light bg-light mb-3 py-0">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <i class="fas fa-tasks">Todo-list React</i>
+        </Link>
+        <div>
+          <div className="navbar-nav">
+            <NavLink className="nav-item nav-link" to="/todos">
+              Todo
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/about">
+              About
+            </NavLink>
+          </div>
         </div>
       </div>
     </nav>
