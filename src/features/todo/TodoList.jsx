f6212c3e70561e6dabb0edb63bd4b88e4ca0184e
Somprasong Damyos <somprasong.damyos@gmail.com>
2019-07-05 09:49:42 +0700

    refactor code

--- src/features/todo/TodoList.jsx
+++ src/features/todo/TodoList.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { Consumer } from '../../context';
 
-function TodoList(props) {
-  const { todos } = props;
+const TodoList = () => {
   return (
-    <>
-      {todos.length === 0 ? (
-        <p className="text-center">No task.</p>
-      ) : (
-        <ul className="list-group">
-          {todos.map(todo => (
-            <TodoItem key={todo.id} todo={todo} />
-          ))}
-        </ul>
-      )}
-    </>
+    <Consumer>
+      {value => {
+        const { todos } = value;
+        return (
+          <>
+            {todos.length === 0 ? (
+              <p className="text-center">No task.</p>
+            ) : (
+              <ul className="list-group">
+                {todos.map(todo => (
+                  <TodoItem key={todo.id} todo={todo} />
+                ))}
+              </ul>
+            )}
+          </>
+        );
+      }}
+    </Consumer>
   );
-}
+};
 
 export default TodoList;
