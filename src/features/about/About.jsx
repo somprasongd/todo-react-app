9dd002c0f61fd698ceba8eea4f0006196c031c63
Somprasong Damyos <somprasong.damyos@gmail.com>
2019-07-05 10:19:27 +0700

    update about page

--- src/features/about/About.jsx
+++ src/features/about/About.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 
-function About() {
+const About = () => {
   return (
     <div>
-      <h1>This is About Page</h1>
+      <h1 className="display-4">About Page</h1>
+
+      <blockquote class="blockquote">
+        <p class="mb-0">
+          Show how to create Todo-list using react -> context api -> hooks
+        </p>
+        <footer class="blockquote-footer">
+          Read more{' '}
+          <cite title="Source Title">
+            <a
+              href="https://github.com/somprasongd/todo-react-app"
+              target="_blank"
+            >
+              <span className="text-danger">On GitHub</span>
+            </a>
+          </cite>
+        </footer>
+      </blockquote>
     </div>
   );
-}
+};
 
 export default About;
